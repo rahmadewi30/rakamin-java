86a88de514d646cb7911205f0163b6642d26c019
rahmadewi30 <rahmadewi30_@hotmail.com>
2023-10-21 11:23:03 +0700

    Create Basic Java Example

--- src/main/java/FirstProgram/perulanganWhile.java
+++ src/main/java/FirstProgram/perulanganWhile.java
@@ -10,7 +10,7 @@ public class perulanganWhile {
         System.out.println("Start");
         int counter = 0;
 
-        while (ulangi.equals(1) || ulangi.equalsIgnoreCase("yes")) {
+        while (ulangi.equalsIgnoreCase("yes")) {
             System.out.println("Pengulangan ke-" + counter);
             System.out.println("Apakah anda mau mengulang? (yes/no): ");
             String jawab = scanner.nextLine();
